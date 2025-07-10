1b9165923bb6b78f71d8edfda493d659539d1c7c
Sathishkumar G <83406165+sathish136@users.noreply.github.com>
2025-07-10 13:43:52 +0530

    Delete old directory

--- old/leave-1/build-client.js
+++
@@ -1,6 +0,0 @@
-const { execSync } = require('child_process');
-const path = require('path');
-
-console.log('Building client...');
-execSync('npm run build:client', { cwd: path.join(__dirname, 'client'), stdio: 'inherit' });
-console.log('Client build completed.');
